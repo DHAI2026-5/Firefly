458a0a6d0491ff4b790df6a00bf4e5553f0854cb
DHAI2026-5 <2502715605@qq.com>
2026-06-22 19:52:47 +0800

    Update siteConfig.ts

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -12,7 +12,7 @@ export const siteConfig: SiteConfig = {
 	subtitle: "在整了别催",
 
 	//站点网址
-	site_url: "https://DHAI202.cuteleaf.cn",
+	site_url: "https://Firefly.cuteleaf.cn",
 
 	// 站点描述
 	description:
