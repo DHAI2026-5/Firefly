db337b136204147753ba6e44b7bf747bbb750a51
DHAI2026-5 <2502715605@qq.com>
2026-06-22 19:56:07 +0800

    Revert "config:前缀修改"
    This reverts commit 0869e6f353726ba7bab1c789728a1bb7f8b3377d.

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -12,8 +12,7 @@ export const siteConfig: SiteConfig = {
 	subtitle: "在整了别催",
 
 	//站点网址
-	site_url: "https://Firefly.cuteleaf.cn",
-
+	site_url: "https://firefly.cuteleaf.cn",
 	// 站点描述
 	description:
 		"Firefly 是一款基于 Astro 框架和 Fuwari 模板开发的清新美观且现代化个人博客主题模板，专为技术爱好者和内容创作者设计。该主题融合了现代 Web 技术栈，提供了丰富的功能模块和高度可定制的界面，让您能够轻松打造出专业且美观的个人博客网站。",
